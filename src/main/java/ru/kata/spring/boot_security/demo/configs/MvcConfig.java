8af168f4b3623145df80ec2445569bb51d33c3f9
User <sidorovn2007@gmail.com>
2026-03-02 18:09:39 +0300

    update

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -6,7 +6,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("login");
+
+        registry.addViewController("/login")
+                .setViewName("login");
+
+        registry.addViewController("/user")
+                .setViewName("user");
+
+        registry.addViewController("/admin")
+                .setViewName("admin");
     }
 }
